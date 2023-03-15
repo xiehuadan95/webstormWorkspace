5e8074be8917fb7fe81b9bca83d139d9504eda7c
eric <694684187@qq.com>
2023-03-15 23:18:47 +0800

    mock - nginx 修改login及自定义url

--- front/srb-admin/src/api/user.js
+++ front/srb-admin/src/api/user.js
@@ -1,23 +1,26 @@
 import request from '@/utils/request'
-
+//登录 登录页面继续用mock的接口 需要手动加baseURL 否则根据导入的request模块 会拼接为 localhost
 export function login(data) {
   return request({
+    baseURL: '/dev-api',
     url: '/vue-admin-template/user/login',
     method: 'post',
     data
   })
 }
-
+//获取用户信息
 export function getInfo(token) {
   return request({
+    baseURL: '/dev-api',
     url: '/vue-admin-template/user/info',
     method: 'get',
     params: { token }
   })
 }
-
+//登出
 export function logout() {
   return request({
+    baseURL: '/dev-api',
     url: '/vue-admin-template/user/logout',
     method: 'post'
   })
